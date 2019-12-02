30c09be17c1e0fe5d48e9bfbd729106c51a39ec4
Matthew Passmore <matthew@matthewpassmore.co.uk>
2019-12-02 20:42:50 +0000

    feat (archives): add toggle list of tags

---
+++ src/_includes/js/main.js
@@ -0,0 +1,35 @@
+(function() {
+
+  /**
+   * Toggle post tags' navigation
+   */
+
+  var button = document.createElement('button');
+  var pageHeader = document.querySelector('.c-archive-header');
+  var pageHeading = document.querySelector('.c-page-heading');
+
+  // add button attributes and content
+  button.setAttribute('aria-expanded', 'false'); 
+  button.setAttribute('id', 'post-filter');
+  button.innerHTML = `
+    Tag list
+    <svg><use xlink:href="#icon-chevron-down"></use></svg>
+  `;
+
+  // insert button into DOM
+  pageHeader.insertBefore(button, pageHeading.nextSibling);
+
+  var menu = button.nextElementSibling; 
+
+  // set initial (closed menu) states
+  button.hidden = false;
+  menu.hidden = true; 
+
+  // toggle menu visibility
+  button.addEventListener('click', function() {
+    var expanded = this.getAttribute('aria-expanded') === 'true';
+    this.setAttribute('aria-expanded', String(!expanded)); menu.hidden = expanded;
+  }); 
+
+})();
+
