535da5a8a44c6525503f039e56384ec30b8960d5
Matthew Passmore <matthew@matthewpassmore.co.uk>
2020-03-11 16:29:23 +0000

    refactor (shortcodes): use partial files

---
+++ src/shortcodes/codetitle.js
@@ -0,0 +1,9 @@
+module.exports = (content) => {
+
+  const response = `
+<div class="c-codetitle">
+  <p><b>Filename: </b>${content}</p>
+</div>`;
+
+  return response;
+};
---
+++ src/shortcodes/reviews.js
@@ -0,0 +1,6 @@
+module.exports = (content) => {
+
+  const response = `<div class="c-post__reviews">${content}</div>`;
+
+  return response;
+};
